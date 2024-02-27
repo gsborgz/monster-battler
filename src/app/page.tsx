48a7cbb97b10053aff903613bf54f90df830c3f1
Gabriel Borges <32948255+gsborgz@users.noreply.github.com>
2024-02-27 23:10:05 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>Teste 1</h1>
+}
---
+++ src/models/type.ts
@@ -0,0 +1,10 @@
+export class Type {
+    id: number;
+    index: number;
+    name: string;
+    color: string;
+    icon: string;
+    weaknesses: number[];
+    resistances: number[];
+    immunities: number[];
+}
